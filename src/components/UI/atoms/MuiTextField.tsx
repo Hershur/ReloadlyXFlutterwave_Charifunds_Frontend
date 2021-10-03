e89f1d8a2a808c30677b2e96c342c9277c3361bc
Assurance Femi <assurancefemi@gmail.com>
2021-10-03 12:53:50 +0100

    Completed mobile view for foundation page

--- src/components/UI/atoms/MuiTextField.tsx
+++
@@ -1,7 +0,0 @@
-import { TextField } from "@material-ui/core";
-
-const MuiTextField = (): JSX.Element => {
-    return <TextField variant="outlined" /*{...fieldToTextField(props)}*/  />;
-};
-
-export default MuiTextField;
--- src/components/UI/organisms/Header.tsx
+++ src/components/UI/organisms/Header.tsx
@@ -1,12 +1,38 @@
-import Logo from "../atoms/Logo"
-import HeaderNav from "../molecules/HeaderNav"
+import { Button, Drawer } from "@mui/material";
+import { useState } from "react";
+import Logo from "../atoms/Logo";
+import HeaderNav from "../molecules/HeaderNav";
+import HeaderNavMobile from "../molecules/HeaderNavMobile";
+import MenuIcon from '@mui/icons-material/Menu';
+
 
 const Header = ():JSX.Element => {
+    const [openDrawer, setOpenDrawer] = useState(false);
+
     return (
         <div className="page-header">
             <Logo />
 
+            {/* <div style={{display: 'flex', justifyContent: 'space-between'}}> */}
+
+                <div className="menu-icon-box">
+                    <Button onClick={()=> setOpenDrawer(!openDrawer)}>
+                        <MenuIcon sx={{color: '#2f5349', fontSize: '2.5rem'}} />
+                    </Button>
+                </div>
+            {/* </div> */}
+
+
             <HeaderNav />
+
+
+            <Drawer
+                anchor='right'
+                open={openDrawer}
+                onClose={()=> setOpenDrawer(!openDrawer)}
+            >
+                <HeaderNavMobile isLandingPage  />
+            </Drawer>
         </div>
     )
 };
