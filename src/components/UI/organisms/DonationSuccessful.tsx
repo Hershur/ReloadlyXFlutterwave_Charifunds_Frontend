9375d95ec4fcaa758909759799237ac774a8ad85
Assurance Femi <assurancefemi@gmail.com>
2021-10-03 15:44:38 +0100

    Completed all mobile views

--- src/components/UI/organisms/DonationSuccessful.tsx
+++ src/components/UI/organisms/DonationSuccessful.tsx
@@ -25,34 +25,38 @@ const DonationSuccessful = (): JSX.Element => {
     };
 
     useEffect(() => {
-        setTimeout(()=> {
-            
-            const duration = 20 * 1000;
-            const animationEnd = Date.now() + duration;
-            const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
 
-            function randomInRange(min: number, max: number) {
-                return Math.random() * (max - min) + min;
-            }
+        if(getDonorNameFromStorage){
 
-            const interval: ReturnType<typeof setInterval> = setInterval(function() {
-                const timeLeft = animationEnd - Date.now();
+            setTimeout(()=> {
+                
+                const duration = 20 * 1000;
+                const animationEnd = Date.now() + duration;
+                const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
 
-                if (timeLeft <= 0) {
-                    return clearInterval(interval);
+                function randomInRange(min: number, max: number) {
+                    return Math.random() * (max - min) + min;
                 }
 
-                var particleCount = 50 * (timeLeft / duration);
-                // since particles fall down, start a bit higher than random
-                confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
-                confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
-            }, 250);
+                const interval: ReturnType<typeof setInterval> = setInterval(function() {
+                    const timeLeft = animationEnd - Date.now();
+
+                    if (timeLeft <= 0) {
+                        return clearInterval(interval);
+                    }
+
+                    var particleCount = 50 * (timeLeft / duration);
+                    // since particles fall down, start a bit higher than random
+                    confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+                    confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+                }, 250);
 
 
-            setOpen(true);
+                setOpen(true);
 
-        }, 2500)
-    }, []);
+            }, 2500)
+        }
+    }, [getDonorNameFromStorage]);
 
 
     if(!getDonorNameFromStorage){
